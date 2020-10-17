9194b787d125891ff0a08484d3082c7c78f62c56
juniordugue <jrdugue@gmail.com>
2020-10-17 08:52:41 -0400

    removed old css, deleted logo, styled the basic form, added states to the form fields

--- src/App.js
+++ src/App.js
@@ -4,11 +4,9 @@ import FormV1 from "./components/formV1/FormV1";
 
 function App() {
   return (
-    <div className="App">
-      <div>
-        <h2>Form version 1</h2>
-        <FormV1 />
-      </div>
+    <div className="login-container">
+      <h2>Form version 1</h2>
+      <FormV1 />
     </div>
   );
 }
--- src/components/formV1/FormV1.js
+++ src/components/formV1/FormV1.js
@@ -1,24 +1,35 @@
-import React from "react";
-import {login} from "./../utils";
+import React, { useState } from "react";
+import { login } from "./../utils";
 
 function FormV1() {
+  const [loading, setLoading] = useState(false);
+  const [username, setUsername] = useState("username");
+  const [password, setPassword] = useState("password");
+
   async function onSubmit(e) {
     e.preventDefault();
+    setLoading(true);
+    console.dir("clicked");
     try {
       await login({});
-      debugger;
-    } catch (error) {}
-    debugger;
+      // debugger;
+    } catch (error) {
+      // debugger;
+    }
+
+    setLoading(false);
   }
   return (
-    <div className="login-container">
+    <>
       <form onSubmit={onSubmit}>
         <p>Please Login</p>
-        <input type="text" placeholder="username" />
-        <input type="text" placeholder="password" />
-        <button type="submit">Log In</button>
+        <input type="text" placeholder="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+        <input type="text" placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        <button type="submit" disabled={loading}>
+          Log In
+        </button>
       </form>
-    </div>
+    </>
   );
 }
 
