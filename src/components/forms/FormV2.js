45c7c85690c7fa659610046469291219863eb748
juniordugue <jrdugue@gmail.com>
2020-10-18 18:39:45 -0400

    Imported use reducer and created a reducer file

--- src/components/forms/FormV2.js
+++ src/components/forms/FormV2.js
@@ -1,7 +1,17 @@
 import React, { useState } from "react";
 import { login } from "../utils";
+import loginReducer from "./reducer";
+
+const initialState = {
+  username: "",
+  password: "",
+}
 
 function FormV2() {
+  // const [username, setUsername] = useState("");
+  // const [password, setPassword] = useState("");
+  const [state, dispatch] = useReducer(reducer, initialState, init)
+
   async function onSubmit(e) {
     e.preventDefault();
     try {
@@ -13,8 +23,8 @@ function FormV2() {
     <>
       <form onSubmit={onSubmit}>
         <p>Intermediate Form</p>
-        <input type="text" placeholder="username" />
-        <input type="password" placeholder="password" />
+        <input type="text" placeholder="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+        <input type="password" placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)} />
         <button type="submit">Log in</button>
       </form>
     </>
